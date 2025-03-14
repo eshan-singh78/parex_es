6c943b8675e0be2cf796586149bed0e4e06d6b33
Eshan Singh <progressivesingh@gmail.com>
2025-03-15 03:35:47 +0530

    cleanup and patch update

--- parex_server.go
+++ parex_server.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
 	"time"
 
@@ -48,56 +46,6 @@ func main() {
 		}
 
 		c.JSON(200, gin.H{"message": "File uploaded successfully", "file_path": filePath})
-	})
-
-	r.POST("/parex_process", func(c *gin.Context) {
-		file, err := c.FormFile("file")
-		if err != nil {
-			c.JSON(400, gin.H{"error": "File not found"})
-			return
-		}
-
-		offsetStr := c.PostForm("offset")
-		levelStr := c.PostForm("level")
-
-		offset, err := strconv.ParseInt(offsetStr, 10, 64)
-		if err != nil {
-			c.JSON(400, gin.H{"error": "Invalid offset"})
-			return
-		}
-
-		level, err := strconv.Atoi(levelStr)
-		if err != nil {
-			c.JSON(400, gin.H{"error": "Invalid level"})
-			return
-		}
-
-		err = os.MkdirAll("./tmp", os.ModePerm)
-		if err != nil {
-			c.JSON(500, gin.H{"error": "Failed to create temp directory"})
-			return
-		}
-
-		filePath := fmt.Sprintf("./tmp/%s", file.Filename)
-		if err := c.SaveUploadedFile(file, filePath); err != nil {
-			c.JSON(500, gin.H{"error": "Failed to save file"})
-			return
-		}
-
-		imageFile, err := os.Open(filePath)
-		if err != nil {
-			c.JSON(500, gin.H{"error": "Failed to open file"})
-			return
-		}
-		defer imageFile.Close()
-
-		err = lib.Explore(imageFile, uint64(offset), level)
-		if err != nil {
-			c.JSON(500, gin.H{"error": "Error processing file", "details": err.Error()})
-			return
-		}
-
-		c.JSON(200, gin.H{"message": "Processing completed successfully"})
 	})
 
 	r.POST("/parex_process_v2", func(c *gin.Context) {
@@ -149,7 +97,6 @@ func main() {
 		}
 		defer out.Close()
 
-		// Redirect stdout to capture output
 		oldStdout := os.Stdout
 		os.Stdout = out
 		err = lib.Explore(imageFile, uint64(offset), level)
@@ -160,10 +107,15 @@ func main() {
 			return
 		}
 
-		// Read and extract filenames from output
-		fileNames, err := extractFileNames(outputFile)
+		fileNames, err := lib.ExtractFileNames(outputFile)
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Failed to extract filenames", "details": err.Error()})
+			return
+		}
+
+		err = lib.CleanTmpDirectory("./tmp")
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Failed to clean tmp directory", "details": err.Error()})
 			return
 		}
 
@@ -174,33 +126,4 @@ func main() {
 	})
 
 	r.Run(":8080")
-}
-
-// extractFileNames extracts filenames from the process output
-func extractFileNames(filePath string) ([]string, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var fileNames []string
-	scanner := bufio.NewScanner(file)
-
-	// Regex to capture filenames at the end of each line
-	re := regexp.MustCompile(`\S+\.\w+$`)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		matches := re.FindString(line)
-		if matches != "" {
-			fileNames = append(fileNames, matches)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return fileNames, nil
 }
