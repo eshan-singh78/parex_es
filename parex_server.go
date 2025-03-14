667606f4d8113b892f1ab88ad1623b777d614f5e
Eshan Singh <progressivesingh@gmail.com>
2025-03-15 03:13:42 +0530

    temporary file upload

--- parex_server.go
+++ parex_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,6 +21,28 @@ func main() {
 
 		currentTime := time.Now().In(loc).Format("2006-01-02 15:04:05 MST")
 		c.JSON(200, gin.H{"message": fmt.Sprintf("Success %s", currentTime)})
+	})
+
+	r.POST("/parex_test", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(400, gin.H{"error": "File not found"})
+			return
+		}
+
+		err = os.MkdirAll("./tmp", os.ModePerm)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Failed to create temp directory"})
+			return
+		}
+
+		filePath := fmt.Sprintf("./tmp/%s", file.Filename)
+		if err := c.SaveUploadedFile(file, filePath); err != nil {
+			c.JSON(500, gin.H{"error": "Failed to save file"})
+			return
+		}
+
+		c.JSON(200, gin.H{"message": "File uploaded successfully", "file_path": filePath})
 	})
 
 	r.Run(":8080")
